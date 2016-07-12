46322f3bec301a6869234a473bde909f92680b08
Matt <Matt@Matt-PC.hsd1.mn.comcast.net>
2016-07-12 16:28:02 -0500

    Starting Project 2.0

---
+++ src/localnote/LocalNote.java
@@ -0,0 +1,35 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package localnote;
+
+import javax.swing.JInternalFrame;
+import javax.swing.JDesktopPane;
+import javax.swing.JMenu;
+import javax.swing.JMenuItem;
+import javax.swing.JMenuBar;
+import javax.swing.JFrame;
+import javax.swing.KeyStroke;
+
+import java.awt.event.*;
+import java.awt.*;
+
+/**
+ *
+ * @author Matt
+ */
+public class LocalNote extends JFrame
+                       implements ActionListener {
+    
+    JDesktopPane Desktop;
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
