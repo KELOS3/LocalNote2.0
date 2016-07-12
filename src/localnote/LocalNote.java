43e7a954216250505b5a75ae699179c19df4ccfe
Matt <Matt@Matt-PC.hsd1.mn.comcast.net>
2016-07-12 17:31:01 -0500

    Whole

--- src/localnote/LocalNote.java
+++ src/localnote/LocalNote.java
@@ -23,13 +23,97 @@ import java.awt.*;
 public class LocalNote extends JFrame
                        implements ActionListener {
     
-    JDesktopPane Desktop;
+    JDesktopPane desktop;
+    
 
-    /**
-     * @param args the command line arguments
-     */
-    public static void main(String[] args) {
-        // TODO code application logic here
+    
+    public LocalNote() {
+        Dimension screenSize = Toolkit.getDefaultToolkit().getScreenSize();
+        this.setSize(screenSize);
+        this.setTitle("Local Note");
+        setDefaultCloseOperation(EXIT_ON_CLOSE);
+        
+        desktop = new JDesktopPane();
+        this.setContentPane(desktop);
+        this.setJMenuBar(createMenues());
+        
+        desktop.setDragMode(JDesktopPane.OUTLINE_DRAG_MODE);
+        
+    }
+            
+
+
+
+    private JMenuBar createMenues() {
+        JMenuBar menuBar = new JMenuBar();
+
+        //Set up the lone menu.
+        JMenu menu = new JMenu("File");
+        menu.setMnemonic(KeyEvent.VK_D);
+        menuBar.add(menu);
+
+        //Set up the first menu item.
+        JMenuItem menuItem = new JMenuItem("New");
+        menuItem.setMnemonic(KeyEvent.VK_N);
+        menuItem.setAccelerator(KeyStroke.getKeyStroke(
+                KeyEvent.VK_N, ActionEvent.ALT_MASK));
+        menuItem.setActionCommand("new");
+        menuItem.addActionListener(this);
+        menu.add(menuItem);
+
+        //Set up the second menu item.
+        menuItem = new JMenuItem("Quit");
+        menuItem.setMnemonic(KeyEvent.VK_Q);
+        menuItem.setAccelerator(KeyStroke.getKeyStroke(
+                KeyEvent.VK_Q, ActionEvent.ALT_MASK));
+        menuItem.setActionCommand("quit");
+        menuItem.addActionListener(this);
+        menu.add(menuItem);
+
+        return menuBar;
+    }
+        //React to menu selections.
+    public void actionPerformed(ActionEvent e) {
+        if ("new".equals(e.getActionCommand())) { //new
+            createFrame();
+        } else if ("quit".equals(e.getActionCommand())) { //new
+            quit();
+        }
     }
     
+        //Create a new internal frame.
+    protected void createFrame() {
+        noteFrame frame = new noteFrame();
+        frame.setVisible(true); //necessary as of 1.3
+        desktop.add(frame);
+        try {
+            frame.setSelected(true);
+        } catch (java.beans.PropertyVetoException e) {}
+    }
+
+    //Quit the application.
+    protected void quit() {
+        System.exit(0);
+    }
+    private static void createAndShowGUI() {
+        //Make sure we have nice window decorations.
+        JFrame.setDefaultLookAndFeelDecorated(true);
+
+        //Create and set up the window.
+        LocalNote frame = new LocalNote();
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+        //Display the window.
+        frame.setVisible(true);
+    }
+
+    public static void main(String[] args) {
+        //Schedule a job for the event-dispatching thread:
+        //creating and showing this application's GUI.
+        javax.swing.SwingUtilities.invokeLater(new Runnable() {
+            public void run() {
+                createAndShowGUI();
+            }
+        });
+    }
 }
